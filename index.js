5fb90e98617b1b2cc436e68cbf2daf946ffc2c1f
thinkingmore <peterkevin823@gmail.comm>
2022-10-25 12:08:49 +0600

    set path to show course by category and course id

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-const courses = require('./data/courses.json')
+const categories = require('./data/courses.json')
+
+const courses = require('./data/details.json')
 
 app.use(cors());
 
@@ -13,16 +15,36 @@ app.get('/',(req,res)=>{
     res.send('api is running');
 })
 
+app.get('/categories',(req,res)=>{
+    res.send(categories);
+})
+
+
+app.get('/categories/:id',(req, res)=>{
+    const id = req.params.id;
+    const course = courses.filter(cs => cs.category_id === id)
+    res.send(course);
+   
+})
+
+// app.get('/categories/:id',(req, res)=>{
+//     const id = req.params.id;
+//     const category = categories.find(cs => cs.id === id)
+//     res.send(category);
+   
+// })
+
 app.get('/courses',(req,res)=>{
     res.send(courses);
 })
 
-app.get('/courses/:id',(req, res)=>{
+
+app.get('/courses/:id',(req,res)=>{
     const id = req.params.id;
-    const course = courses.find(cs => cs.id === id)
-    res.send(course);
-   
+    const singleCourse = courses.find(sc=> sc.id === id)
+    res.send(singleCourse);
 })
+
 
 app.listen(port, ()=>{
     console.log('educademy is running');
